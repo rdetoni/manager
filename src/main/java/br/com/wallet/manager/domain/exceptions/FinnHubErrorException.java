5a04cddc2be0aa778d0a95ce64df185d66ddbf89
Ricardo <ricardo.detoni@gmail.com>
2025-08-02 16:46:20 -0300

    | Creates new exception, used to handle FinnHub errors

---
+++ src/main/java/br/com/wallet/manager/domain/exceptions/FinnHubErrorException.java
@@ -0,0 +1,7 @@
+package br.com.wallet.manager.domain.exceptions;
+
+public class FinnHubErrorException extends Exception{
+    public FinnHubErrorException(String message) {
+        super(message);
+    }
+}
--- src/main/java/br/com/wallet/manager/service/FinnhubService.java
+++ src/main/java/br/com/wallet/manager/service/FinnhubService.java
@@ -1,11 +1,15 @@
 package br.com.wallet.manager.service;
 
+import br.com.wallet.manager.domain.exceptions.FinnHubErrorException;
 import br.com.wallet.manager.model.entities.FinnHubQuote;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -24,9 +28,15 @@ public class FinnhubService {
     }
 
     @Cacheable("quotes")
-    public FinnHubQuote getQuote(String symbol) {
+    public FinnHubQuote getQuote(String symbol) throws FinnHubErrorException{
         log.info("Getting latest price for {}", symbol);
         String url = String.format("%s/quote?symbol=%s&token=%s", baseUrl, symbol, apiKey);
-        return restTemplate.getForObject(url, FinnHubQuote.class);
+        val response = Optional.ofNullable(restTemplate.getForObject(url, FinnHubQuote.class))
+                .orElseThrow(() -> new FinnHubErrorException("Error obtaining FinnHub quote"));
+        if(response.getC() == 0){
+            throw new FinnHubErrorException("No current value could be obtained for " + symbol);
+        } else {
+            return response;
+        }
     }
 }
